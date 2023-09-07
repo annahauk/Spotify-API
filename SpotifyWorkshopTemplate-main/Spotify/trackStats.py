aed836d986eb4a56e2dcbfeb6ccbf30854a64f53
Anna Hauk <107438528+annahauk@users.noreply.github.com>
2023-09-07 18:58:30 -0500

    Add files via upload

---
+++ SpotifyWorkshopTemplate-main/Spotify/trackStats.py
@@ -0,0 +1,5 @@
+#playlists on mood
+#Application Programming Interface
+# enables 2 software components to communicate with each other using a set of definitions and protocols
+
+#Spotfiy API is a restful API
